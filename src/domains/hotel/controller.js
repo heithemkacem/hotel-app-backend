51fe567fcd6490ba9c9bad80f7202cf77960f3e8
Heithem Kacem <heithemkacem2016@gmail.com>
2023-01-16 14:16:45 +0000

    adding hotels crud

--- src/domains/hotel/controller.js
+++ src/domains/hotel/controller.js
@@ -14,4 +14,44 @@ const GetHotelById = async (id) => {
     throw error;
   }
 };
-module.exports = { GetHotelById };
+//Admin get all hotels
+const getAllHotels = async () => {
+  try {
+    const allHotels = await Hotel.find();
+    if (!allHotels.length) {
+      return null;
+    }
+
+    return allHotels;
+  } catch (error) {
+    throw error;
+  }
+};
+//Admin delete Hotel by id
+const deleteHotel = async (id) => {
+  try {
+    const deletedHotel = await Hotel.findByIdAndDelete(id);
+    return deletedHotel;
+  } catch (error) {
+    throw error;
+  }
+};
+//Admin update Hotel by id
+const updateHotel = async (id, data) => {
+  try {
+    //find the hotel by id
+    const hotel = await Hotel.findById(id);
+    if (!hotel) {
+      throw Error("common:Hotel_not_found");
+    }
+    //update the hotel
+    const updatedHotel = await Hotel.findByIdAndUpdate(id, data, {
+      new: true,
+    });
+    return updatedHotel;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { GetHotelById, getAllHotels, updateHotel, deleteHotel };
--- src/domains/hotel/routes.js
+++ src/domains/hotel/routes.js
@@ -4,7 +4,12 @@ const router = express.Router();
 //Passport
 const passport = require("passport");
 const { strategy } = require("./../../security/strategy");
-const { GetHotelById } = require("./controller");
+const {
+  GetHotelById,
+  getAllHotels,
+  updateHotel,
+  deleteHotel,
+} = require("./controller");
 router.use(passport.initialize());
 passport.use(strategy);
 router.post("/findHotel", async (req, res) => {
@@ -38,6 +43,68 @@ router.post("/findHotel", async (req, res) => {
         hotelStars,
         hotelRooms,
       },
+    });
+  } catch (error) {
+    res.json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+});
+//Admin Get All Hotels
+router.get("/hotels", async (req, res) => {
+  try {
+    const allHotels = await getAllHotels();
+
+    if (allHotels !== null) {
+      res.json({
+        status: "Success",
+        message: "Hotels Found",
+        hotels: allHotels,
+      });
+    } else {
+      res.json({
+        status: "Success",
+        message: "No Hotels Found",
+        hotels: [],
+      });
+    }
+  } catch (error) {
+    res.json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+});
+
+//Admin Delete Hotel
+router.delete("/delete-hotel/:id", async (req, res) => {
+  try {
+    const deletedHotel = await deleteHotel(req.params.id);
+    res.json({
+      status: "Success",
+      message: "Hotel Deleted",
+      hotel: deletedHotel,
+    });
+  } catch (error) {
+    res.json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+});
+//Admin Update Hotel
+router.put("/update-hotel/:id", async (req, res) => {
+  try {
+    const { error } = hotelRegisterValidation(req.body);
+    if (error) {
+      throw new Error(error["details"][0]["message"]);
+    }
+    const updatedHotel = await updateHotel(req.params.id, req.body);
+    res.json({
+      status: "Success",
+      message: "Hotel Updated",
+      hotel: updatedHotel,
     });
   } catch (error) {
     res.json({
